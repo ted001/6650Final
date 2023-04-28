9db384b350fe73ccf60142d54f2101a7ea8512bb
MarshmelloRice <xinxin99661@gmail.com>
2023-04-28 19:53:55 -0400

    add lock config

---
+++ src/main/java/com/educative/ecommerce/config/LockConfig.java
@@ -0,0 +1,22 @@
+package com.educative.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.jdbc.lock.DefaultLockRepository;
+import org.springframework.integration.jdbc.lock.JdbcLockRegistry;
+import org.springframework.integration.jdbc.lock.LockRepository;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LockConfig {
+    @Bean
+    public DefaultLockRepository DefaultLockRepository(DataSource dataSource){
+        return new DefaultLockRepository(dataSource);
+    }
+
+    @Bean
+    public JdbcLockRegistry jdbcLockRegistry(LockRepository lockRepository){
+        return new JdbcLockRegistry(lockRepository);
+    }
+}
--- src/main/java/com/educative/ecommerce/controller/CategoryController.java
+++ src/main/java/com/educative/ecommerce/controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.educative.ecommerce.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.integration.support.locks.LockRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 
 @RestController
 @RequestMapping("/category")
@@ -21,6 +24,11 @@ public class CategoryController {
 
     @Autowired
     CategoryService categoryService;
+    private final LockRegistry lockRegistry;
+
+    public CategoryController(LockRegistry lockRegistry) {
+        this.lockRegistry = lockRegistry;
+    }
 
     @PostMapping("/create")
     public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category) {
@@ -36,10 +44,19 @@ public class CategoryController {
     @PostMapping("/update/{categoryId}")
     public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryId") int categoryId, @RequestBody Category category ) throws Exception {
         System.out.println("category id " + categoryId);
-        if (!categoryService.findById(categoryId)) {
-            return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category does not exists"), HttpStatus.NOT_FOUND);
+        Lock lock = lockRegistry.obtain(categoryId);
+
+        if (lock.tryLock(1000, TimeUnit.SECONDS)) {
+            try {
+                if (!categoryService.findById(categoryId)) {
+                    return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category does not exists"), HttpStatus.NOT_FOUND);
+                }
+                categoryService.editCategory(categoryId, category);
+                return new ResponseEntity<ApiResponse>(new ApiResponse(true, "category has been updated"), HttpStatus.OK);
+            } finally {
+                lock.unlock();
+            }
         }
-        categoryService.editCategory(categoryId, category);
-        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "category has been updated"), HttpStatus.OK);
+        return null;
     }
 }
